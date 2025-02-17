e0e4b23256cb56d010bffd730128e8e89b31a8b8
rennymartinarise <rennymartinarise@gmail.com>
2025-02-17 18:50:42 +0000

    te

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,32 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable all ESLint rules
+      "no-console": "off",
+      "no-debugger": "off",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+      "no-redeclare": "off",
+      "no-alert": "off",
+      "no-eval": "off",
+      "no-duplicate-imports": "off",
+      "no-implicit-coercion": "off",
+      "no-new-func": "off",
+      "no-shadow": "off",
+      "no-empty": "off",
+      "eqeqeq": "off",
+      "curly": "off",
+      "consistent-return": "off",
+      "semi": "off",
+      "quotes": "off",
+      "indent": "off",
+      "linebreak-style": "off",
+      "react/react-in-jsx-scope": "off", // for React 18 or higher
+      // Add any other specific rules you want to disable
+    },
+  },
 ];
 
 export default eslintConfig;
